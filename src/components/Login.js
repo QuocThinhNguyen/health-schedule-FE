566f3080e13f1d710fbc3068a950b07ec626af25
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-08-29 22:07:48 +0700

    register

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,19 +15,20 @@ function Login() {
                 password: password
             }).then((res) => {
                 console.log(res.data);
+                console.log(res.data.result.authenticated);
+
+                if (res.data.result.authenticated === true) {
+                    alert("Login successful");
+                }
             });
         } catch (err) {
             if (err.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
                 console.log(err.response.data);
                 console.log(err.response.status);
                 console.log(err.response.headers);
             } else if (err.request) {
-                // The request was made but no response was received
                 console.log(err.request);
             } else {
-                // Something happened in setting up the request that triggered an Error
                 console.log('Error', err.message);
             }
             console.log(err.config);
@@ -53,11 +54,6 @@ function Login() {
                                     onChange={(event) => {
                                         setUsername(event.target.value);
                                     }}
-                                // value={email}
-                                // onChange={(event) => {
-                                //   setEmail(event.target.value);
-                                // }}
-
                                 />
                             </div>
                             <div class="form-group">
@@ -67,11 +63,6 @@ function Login() {
                                     onChange={(event) => {
                                         setPassword(event.target.value);
                                     }}
-                                // value={password}
-                                // onChange={(event) => {
-                                //   setPassword(event.target.value);
-                                // }}
-
                                 />
                             </div>
                             <button type="submit" class="btn btn-primary"
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    {/* <Login /> */}
+    <Register />
   </React.StrictMode>
 );
 
