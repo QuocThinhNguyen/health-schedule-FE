3e118366620815d6e31436ed6ca371ae644cc94c
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2025-01-24 23:04:43 +0700

    dashboard user

--- src/pages/User/UserDashboard.jsx
+++ src/pages/User/UserDashboard.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate, Outlet } from 'react-router-dom';
 import Sidebar from '~/components/SidebarUser/Sidebar'; // Adjust the path to your Sidebar component
-import UserProfile from './UserProfile';
-import AppointmentManagement from './AppointmentManagement';
-import PatientRecords from './PatientRecords';
-import ChangePassword from './ChangePassword';
 
 const UserDashboard = () => {
     const [selectedTab, setSelectedTab] = useState('profile'); // Default to 'profile'
